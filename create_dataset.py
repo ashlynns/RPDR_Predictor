d040602d6a684422ad8c1e8d8e0f93d48b33bde7
ashlynns <ashlynn.steeves@icloud.com>
2020-11-25 12:12:09 -0800

    added normalization

--- create_dataset.py
+++ create_dataset.py
@@ -9,6 +9,7 @@ dataset = contestants[['contestant_id', 'season_number', 'age', 'hometown_city',
 lat = []
 lng = []
 
+## retriving lat/long co-ordinates for hometowns
 for i in range(0,len(dataset)):
 	town = dataset.hometown_city.iloc[i]
 	state = dataset.hometown_state.iloc[i]
@@ -18,10 +19,10 @@ for i in range(0,len(dataset)):
 			t = cities[(cities['city_ascii'] == town) & (cities['state_name'] == state)]
 			lat.append(t.lat.values[0])
 			lng.append(t.lng.values[0])
-
 dataset.insert(5, 'Lat', lat)
 dataset.insert(6, 'Lng', lng) 
 
+## 
 def performance_stats(id):
 	id_data = rankings[rankings['contestant_id']==id]
 	n = len(id_data)
@@ -49,9 +50,8 @@ for i in dataset.contestant_id:
 	low.append(low_id)
 	btm.append(btm_id)
 
-
 dataset.insert(7, 'Wins', wins)
-dataset.insert(8, 'Highs', highs)
+dataset.insert(8, 'High', highs)
 dataset.insert(9, 'Safe', safe)
 dataset.insert(10, 'Low', low)
 dataset.insert(11, 'Bottom', btm)
@@ -62,8 +62,7 @@ contestants_per_szn_dct = {}
 for s in dataset.season_number.unique():
 	contestants_per_szn_dct[s] = len(dataset[dataset['season_number']==s])
 
-
-#smaller value means they performerd better
+#smaller value means they performed better
 fraction_label = dataset.season_outcome/[contestants_per_szn_dct[x] for x in dataset.season_number]
 
 labels = [] # This is written to divide into 4 performance categories 
@@ -79,6 +78,34 @@ for f in fraction_label:
 	if f > 4/5:
 		labels.append(5)
 
-
 dataset.insert(14, 'Label', labels)
+
+
+### Normalization ### 
+
+def minmaxnorm(data):
+	minv = min(data)
+	#print(minv)
+	maxv = max(data)
+	#print(maxv)
+	scaled = (2*(data-minv)/(maxv-minv))-1 
+	return(scaled)
+
+def norm(column, dataset=dataset): 
+	n = 'Number_of_Episodes'
+	data = dataset[column]
+	data_n1 = data/dataset[n] # normalized by episode apperances
+	#print(data_n1.unique())
+	data_n2 = minmaxnorm(data_n1) # min max scaling 
+	return(data_n2)
+
+#norm('Bottom')	
+
+dataset.insert(3, 'age_n', norm('age'))
+dataset.insert(9, 'Wins_n', norm('Wins'))
+dataset.insert(11, 'High_n', norm('High'))
+dataset.insert(13, 'Safe_n', norm('Safe'))
+dataset.insert(15, 'Low_n', norm('Low'))
+dataset.insert(17, 'Bottom_n', norm('Bottom'))
+
 dataset.to_csv('Datasets/RPDR_Predictor_Dataset.csv', index=False)
